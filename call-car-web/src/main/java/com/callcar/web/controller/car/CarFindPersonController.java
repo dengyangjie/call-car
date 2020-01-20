6eff0f156f29614c7f200d39bf7391b44bf987af
Dengyangjie <739891618dyj>
2020-01-20 16:46:55 +0800

    项目搭建

---
+++ call-car-web/src/main/java/com/callcar/web/controller/car/CarFindPersonController.java
@@ -0,0 +1,17 @@
+package com.callcar.web.controller.car;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/car")
+public class CarFindPersonController {
+    /**
+     * 车找人信息注册
+     * */
+    public String carFindPerson(){
+        return  null;
+    }
+}
